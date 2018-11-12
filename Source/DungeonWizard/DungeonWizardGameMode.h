8d246a8bbc34a332eef1da450d6f85c6669d5def
Ferenc Rozsa <FerencRozsa@users.noreply.github.com>
2018-11-12 22:37:03 +0000

    Initial commit

---
+++ Source/DungeonWizard/DungeonWizardGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DungeonWizardGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADungeonWizardGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADungeonWizardGameMode();
+};
+
+
+
