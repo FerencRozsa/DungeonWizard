647b14c3694fd24f761e52ed61ffcd794d8012bf
Ferenc Rozsa <43654758+FerencRozsa@users.noreply.github.com>
2018-12-13 19:42:45 +0000

    WizardDungeon abilities
    made 2 abilities with particles and blended animations together to work smoothly

---
+++ Source/DungeonWizard/WizardAbility.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WizardAbility.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/SphereComponent.h"
+#include "Engine/World.h"
+
+UWizardAbility::UWizardAbility()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+void UWizardAbility::BeginPlay()
+{
+	Super::BeginPlay();	
+
+}
+
+
+void UWizardAbility::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (canBeUsedAbilityOne == false)
+	{
+		CooldownAbilityOne -= DeltaTime;
+
+		if (CooldownAbilityOne <= 0)
+		{
+			CooldownAbilityOne = SetCooldownOne;
+			canBeUsedAbilityOne = true;
+		}
+	}
+
+	if (canBeUsedAbilityTwo == false)
+	{
+		CooldownAbilityTwo -= DeltaTime;
+
+		if (CooldownAbilityTwo <= 0)
+		{
+			CooldownAbilityTwo = SetCooldownTwo;
+			canBeUsedAbilityOne = true;
+		}
+	}
+
+}
+
+void UWizardAbility::AbilityOne()
+{
+	UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetOwner()->GetActorTransform());
+	UGameplayStatics::PlaySoundAtLocation(GetWorld(), AudioEffect, GetOwner()->GetActorLocation());
+	canBeUsedAbilityOne = false;
+}
+
+void UWizardAbility::AbilityTwo()
+{
+	UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetOwner()->GetActorTransform());
+	UGameplayStatics::PlaySoundAtLocation(GetWorld(), AudioEffect, GetOwner()->GetActorLocation());
+	canBeUsedAbilityTwo = false;
+}
